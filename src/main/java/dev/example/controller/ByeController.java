9b4ec288eb2d8a71bfc429585161be3e1711a4b9
yes2489 <yes24150@gmail.com>
2025-09-25 17:20:50 +0900

    feat: add ByeController

---
+++ src/main/java/dev/example/controller/ByeController.java
@@ -0,0 +1,15 @@
+package dev.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bye")
+public class ByeController {
+
+    @GetMapping
+    public String bye(){
+        return "bye";
+    }
+}
